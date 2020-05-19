fd8e0b4e560cc76b122b377010590695dfc24f4c
CarlosGutierrezArdila <chgutierreza@correo.udistrital.edu.co>
2020-05-19 15:44:10 -0500

    Assignment 1

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,71 +1,124 @@
 import React, { Component } from 'react'
-import Menu from './MenuComponent'
-import Home from './HomeComponent'
 import DishDetails from './DishdetailComponent'
 import { View, Platform } from 'react-native'
-import { createStackNavigator , createDrawerNavigator} from 'react-navigation'
+import { createStackNavigator, createDrawerNavigator } from 'react-navigation'
 import Constants from 'expo-constants'
+//components
+import Menu from './MenuComponent'
+import Home from './HomeComponent'
+import Contact from './ContactComponent'
+import About from './AboutComponent'
+
+
 // stack navigacion
 const MenuNavigator = createStackNavigator({
-    Menu: { screen: Menu },
-    DishDetails: { screen: DishDetails }
+  Menu: { screen: Menu },
+  DishDetails: { screen: DishDetails }
 }, {
-    initialRouteName: 'Menu',
-    navigationOptions: {
-        headerStyle: {
-            backgroundColor: "#512DA8"
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-            color: "#fff"            
-        }
+  initialRouteName: 'Menu',
+  navigationOptions: {
+    headerStyle: {
+      backgroundColor: "#512DA8"
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      color: "#fff"
     }
+  }
 })
-//drawer navigation
+
+// home
 
 const HomeNavigator = createStackNavigator({
-    Home: { screen: Home }
-  }, {
-    navigationOptions: ({ navigation }) => ({
-      headerStyle: {
-          backgroundColor: "#512DA8"
-      },
-      headerTitleStyle: {
-          color: "#fff"            
-      },
-      headerTintColor: "#fff"  
-    })
+  Home: { screen: Home }
+}, {
+  navigationOptions: ({ navigation }) => ({
+    headerStyle: {
+      backgroundColor: "#512DA8"
+    },
+    headerTitleStyle: {
+      color: "#fff"
+    },
+    headerTintColor: "#fff"
+  })
+})
+// contact us
+const ContactNavigator = createStackNavigator({
+  Contact: { screen: Contact }
+}, {
+  navigationOptions: ({ navigation }) => ({
+    headerStyle: {
+      backgroundColor: "#512DA8"
+    },
+    headerTitleStyle: {
+      color: "#fff"
+    },
+    headerTintColor: "#fff"
+  })
+})
+//about us
+const AboutNavigator = createStackNavigator({
+  About: { screen: About }
+}, {
+  navigationOptions: ({ navigation }) => ({
+    headerStyle: {
+      backgroundColor: "#512DA8"
+    },
+    headerTitleStyle: {
+      color: "#fff"
+    },
+    headerTintColor: "#fff"
+  })
 })
 
+// main drawer side navigator
+
 const MainNavigator = createDrawerNavigator({
-    Home: 
-      { screen: HomeNavigator,
-        navigationOptions: {
-          title: 'Home',
-          drawerLabel: 'Home'
-        }
-      },
-    Menu: 
-      { screen: MenuNavigator,
-        navigationOptions: {
-          title: 'Menu',
-          drawerLabel: 'Menu'
-        }, 
-      }
+  Home:
+  {
+    screen: HomeNavigator,
+    navigationOptions: {
+      title: 'Home',
+      drawerLabel: 'Home'
+    }
+  },
+  About:
+  {
+    screen: AboutNavigator,
+    navigationOptions: {
+      title: 'About Us',
+      drawerLabel: 'About Us'
+    }
+  },
+  Menu:
+  {
+    screen: MenuNavigator,
+    navigationOptions: {
+      title: 'Menu',
+      drawerLabel: 'Menu'
+    },
+  },
+  Contact: {
+    screen: ContactNavigator,
+    navigationOptions: {
+      title: 'Contact Us',
+      drawerLabel: 'Contact Us'
+    },
+  }
 }, {
   drawerBackgroundColor: '#D1C4E9'
 })
 
 class Main extends Component {
 
-    render() {
-        return (
-            <View style={{ flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight}}>
-                <MainNavigator/>
-            </View>
+  render() {
+    return (
+      <View style={{ flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight }}>
+        <MainNavigator />
+      </View>
 
-        )
-    }
+    )
+  }
 }
 
 export default Main
