3cb53e313800d44a4bf88bc2980d8ed49e76fd5b
CarlosGutierrezArdila <chgutierreza@correo.udistrital.edu.co>
2020-05-25 21:48:03 -0500

    loading component and errmess

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
-import { View, FlatList } from 'react-native'
+import { View, FlatList, Text } from 'react-native'
 import { Tile } from 'react-native-elements'
 import { connect } from 'react-redux'
 import { baseUrl } from '../shared/baseUrl'
+import { Loading } from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -29,19 +30,34 @@ class Menu extends Component {
                     caption={item.description}
                     featured
                     onPress={() => navigate('DishDetails', { dishId: item.id })} //navigate and pass info
-                    // image={{ uri: baseUrl + item.image }} 
+                    image={{ uri: baseUrl + item.image }} 
                     />
             )
         }
 
         const { navigate } = this.props.navigation
 
-        return (
-            <FlatList
-                data={this.props.dishes.dishes}
-                renderItem={renderMenuItem}
-                keyExtractor={item => item.id.toString()} />
-        )
+        if (this.props.dishes.isLoading) {
+            return(
+                <Loading />
+            );
+        }
+        else if (this.props.dishes.errMess) {
+            return(
+                <View>            
+                    <Text>{this.props.dishes.errMess}</Text>
+                </View>            
+            );
+        }
+        else {
+            return (
+                <FlatList 
+                    data={this.props.dishes.dishes}
+                    renderItem={renderMenuItem}
+                    keyExtractor={item => item.id.toString()}
+                    />
+            );
+        }
     }
 
 
